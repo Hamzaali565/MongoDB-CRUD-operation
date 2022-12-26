0657e6214dbff938d77200cc0848184d4d37e2d3
Hamzaali565 <ha6153806@gmail.com>
2022-12-26 14:49:48 -0800

    jwt updates

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,28 +1,47 @@
 import logo from './logo.svg';
 // import './App.css';
 import Navbar from './components/Navbar';
+import Signup from './components/Signup'
+import Login from './components/Login'
 import { Stack } from '@mui/system';
 import { useState } from 'react';
 import { Box, createTheme } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 
 function App() {
-  // const [mode, setMode] = useState("dark")
-  // const darkTheme = createTheme({
-  //   palette: {
-  //     mode: mode
-  //   }
-  // })
+  const [isLogin, setIsLogin] = useState(false);
+  const [fullName, setFullName] = useState("");
+
   return (
-    // <ThemeProvider theme={darkTheme}>
-      // <Box>
-        // <Box>
-          <Stack>
-            <Navbar />
-          </Stack>
-        // </Box>
-      // </Box>
-    // </ThemeProvider>
+
+    <Stack>
+      {
+        (isLogin) ?
+          null
+          :
+          <ul className='navBar'>
+            <li> <Link to={`/`}>Login</Link> </li>
+            <li> <Link to={`/signup`}>Signup</Link> </li>
+          </ul>
+      }
+
+
+      {(isLogin) ?
+
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Routes>
+        :
+        <Routes>
+          <Route path="/" element={<Login set={setIsLogin} />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Routes>
+      }
+    </Stack>
+
   );
 }
 
