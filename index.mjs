d0f2753c481b0d8e5fb3df087b380b31a1f1c9a5
Hamzaali565 <ha6153806@gmail.com>
2022-12-26 15:55:10 -0800

    Jwt Updates

--- index.mjs
+++ index.mjs
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:3001', "*"],
+    origin: ['http://localhost:3000', "*"],
     credentials: true
 }));
 
